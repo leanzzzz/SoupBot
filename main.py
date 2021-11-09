bbebcb3cc5c1ac8fe544588104242142e40f41d0
leanzzz <92376858+leanzzzz@users.noreply.github.com>
2021-11-09 09:15:46 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from keep_alive import keep_alive
+from discord.ext import commands
+
+bot = commands.Bot(
+	command_prefix="!",  # Change to desired prefix
+	case_insensitive=True  # Commands aren't case-sensitive
+)
+
+bot.author_id = 487258918465306634  # Change to your discord id!!!
+
+@bot.event 
+async def on_ready():  # When the bot is ready
+    print("I'm in")
+    print(bot.user)  # Prints the bot's username and identifier
+
+
+extensions = [
+	'cogs.cog_example'  # Same name as it would be if you were importing it
+]
+
+if __name__ == '__main__':  # Ensures this is the file being ran
+	for extension in extensions:
+		bot.load_extension(extension)  # Loades every extension.
+
+keep_alive()  # Starts a webserver to be pinged.
+bot.run('OTA3NDE5MDY1MDEyODU0ODY0.YYm53A.ajJei9NF5XNiwo_w3JqeF3wzMu8')  # Starts the bot
